1fa4e028b4ce4a9902c37c010c87e670b33398ec
Subhasish58 <166903452+Subhasish58@users.noreply.github.com>
2025-06-19 16:47:35 +0530

    register and login functionality

--- client/src/App.js
+++ client/src/App.js
@@ -2,9 +2,11 @@ import { Routes, Route } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import Login from "./pages/auth/Login";
 import Register from "./pages/auth/Register";
+import { ToastContainer } from "react-toastify";
 function App() {
   return (
     <>
+      <ToastContainer />
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/login" element={<Login />} />
