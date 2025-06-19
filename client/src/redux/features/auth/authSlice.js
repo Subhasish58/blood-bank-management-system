81b065589b8682fdaebbc3150c2822960c38dfe1
Subhasish58 <166903452+Subhasish58@users.noreply.github.com>
2025-06-19 21:17:53 +0530

    public and protected routes configured

--- client/src/redux/features/auth/authSlice.js
+++ client/src/redux/features/auth/authSlice.js
@@ -45,18 +45,18 @@ const authSlice = createSlice({
             state.error = payload;
         });
         // CURRENT user
-        // builder.addCase(getCurrentUser.pending, (state) => {
-        //   state.loading = true;
-        //   state.error = null;
-        // });
-        // builder.addCase(getCurrentUser.fulfilled, (state, { payload }) => {
-        //   state.loading = false;
-        //   state.user = payload.user;
-        // });
-        // builder.addCase(getCurrentUser.rejected, (state, { payload }) => {
-        //   state.loading = false;
-        //   state.error = payload;
-        // });
+        builder.addCase(getCurrentUser.pending, (state) => {
+            state.loading = true;
+            state.error = null;
+        });
+        builder.addCase(getCurrentUser.fulfilled, (state, { payload }) => {
+            state.loading = false;
+            state.user = payload.user;
+        });
+        builder.addCase(getCurrentUser.rejected, (state, { payload }) => {
+            state.loading = false;
+            state.error = payload;
+        });
     }
 });
 
